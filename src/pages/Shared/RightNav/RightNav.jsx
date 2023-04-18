a030e84f398ed1d505d11635ef51a23b91499163
sabuj0143 <sabujhowlader011@gmai.com>
2023-04-19 01:24:58 +0600

    API of localsite and react icons added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>RightNav is here</h2>
+            <h4>Login With</h4>
+            <Button className='my-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"> <FaGithub /> Login with Github</Button>
+            <div>
+                <h4 className='mt-4'>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item className='mt-2' disabled> <FaFacebookF /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img className='position-absolute' src={bg} alt="" />
+                <div className='position-relative text-white text-center p-4 mt-8 px-4'>
+                    <p>Create an <br /> Amazing Newspaper</p>
+                    <p><small>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</small></p>
+                    <Button variant="danger">Learn More</Button>
+                </div>
+            </div>
         </div>
     );
 };
