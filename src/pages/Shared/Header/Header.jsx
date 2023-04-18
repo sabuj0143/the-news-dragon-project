a425bbb8a1b043db15cea57554abcfe1c46a93da
sabuj0143 <sabujhowlader011@gmai.com>
2023-04-18 23:25:43 +0600

    moment time added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import logo from '../../../assets/logo.png'
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favor</small></p>
-                <p>Sunday, November 27, 2022</p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={50}>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+                </Marquee>
+            </div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
