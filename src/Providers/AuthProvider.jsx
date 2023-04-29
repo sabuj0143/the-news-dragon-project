bd1138e84e789efae6839c6edb09fcc1546f55bb
sabuj0143 <sabujhowlader011@gmai.com>
2023-04-29 23:15:50 +0600

    PrivateRoute and signIn and signOut added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,20 +1,43 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const user = null;
+    const [user, setUser] = useState(null)
 
+    // Create a new user
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    // Login user
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // User Log Out
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    // use ase ki nai check 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log('login in user', loggedUser);
+            setUser(loggedUser);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
         user,
-        createUser
+        createUser,
+        signIn,
+        logOut
     }
     return (
         <AuthContext.Provider value={authInfo}>
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -21,6 +21,7 @@ const Register = () => {
         .then(result => {
             const createdUser = result.user;
             console.log(createdUser);
+            form.reset('');
         })
         .catch(error => {
             console.log(error);
