7750ceb06581d751f89f25e8ae53587d9a4ab395
sabuj0143 <sabujhowlader011@gmai.com>
2023-04-30 20:20:53 +0600

    Environment variable for Firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAERLibVreC29W87WVVT_vKiSksV3W6yow",
-  authDomain: "the-new-dragon-project.firebaseapp.com",
-  projectId: "the-new-dragon-project",
-  storageBucket: "the-new-dragon-project.appspot.com",
-  messagingSenderId: "464360574236",
-  appId: "1:464360574236:web:367795dc009e2e6f85fed9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
